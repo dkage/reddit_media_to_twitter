faac3724df002941d2dc574db9ec5be9b2c1e40e
Danilo Cunha <danilokage@gmail.com>
2021-05-06 22:38:32 -0300

    Add docstrings and print log function for updates

--- telegram.py
+++ telegram.py
@@ -15,6 +15,11 @@ class Telegram:
         self.update_offset = int()
 
     def get_me(self):
+        """
+        This function requests the getMe endpoint, which sends the information about
+        the current API_KEY being used, like bot_id, and name of the bot.
+        :return:
+        """
 
         url = self.base_url + 'getMe'
         print('\nTrying getMe')
@@ -29,6 +34,11 @@ class Telegram:
             print("Error during request. Status code {}\n".format(response.status_code))
 
     def get_updates(self):
+        """
+        Get the new messages the bot received on chat. The JSON containing the chat and text info are called
+        by the API, "updates".
+        :return: array with bot last received updates
+        """
 
         print('\nGetting updates.')
         updates_url = self.base_url + 'getUpdates?timeout=100&limit=10'
@@ -36,22 +46,34 @@ class Telegram:
             updates_url = updates_url + '&offset={}'.format(str(self.update_offset))
 
         response = requests.get(updates_url)
-        if requests.status_codes == 200:
+        if response.status_code == 200:
             print('Updates received. Working on them now.\n')
-            self.update_offset = response.json()['result'][-1]['update_id'] + 1
+            updates_array = response.json()['result']
+            self.update_offset = updates_array[-1]['update_id'] + 1
+            print(updates_array[0])
 
-            return response.json()['result']
+            return updates_array
         else:
             print("Error during request. Status code {}\n".format(response.status_code))
 
     def send_message(self, chat_id, text_message):
+        """
+        Sends a text message for the chat with the param ID
+
+        :param chat_id: ID of chat, identifier for a chat with a given user or a group
+        :param text_message: message to be sent
+        :return:
+        """
         chat_id = 15746192
         text_message = 'testing, hello DK'
 
         message_as_json = self.format_json(text_message, chat_id)
         response = requests.get(self.base_url + 'sendMessage', data=message_as_json)
-        print(response)
 
+        if response.status_code == 200:
+            print('Message was sent for the chat {}'.format(chat_id))
+        else:
+            print('Error sending message to chat {}'.format(chat_id))
 
     @staticmethod
     def format_json(text_message, chat_id):
@@ -62,6 +84,15 @@ class Telegram:
 
         return message_to_send_data
 
+    @staticmethod
+    def update_log(single_update):
+        message = '\nUpdate ID {} from Chat ID {}: \n' \
+                  'User {}, which has the id {}, has sent the following message:\n' \
+                  '{}'.format(single_update['update_id'],
+                              single_update['message']['chat']['id'],
+                              single_update['message']['from']['username'],
+                              single_update['message']['from']['id'],
+                              single_update['message']['text'])
+        print(message)
 
-    def check_response(self):
-        pass
+        return message
