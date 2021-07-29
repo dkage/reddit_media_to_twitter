99783cc2a8679a448e65c214febcaf81f74d40ba
Danilo Cunha <danilokage@gmail.com>
2021-07-29 00:41:46 -0300

    Reddit.py - Add functions prototypes and possible checks and logic for each

--- reddit.py
+++ reddit.py
@@ -6,29 +6,31 @@ from api_keys import reddit_id, reddit_secret
 class RedditHandler:
     def __init__(self, url):
         self.reddit = praw.Reddit(client_id=reddit_id,
-                             client_secret=reddit_secret,
-                             user_agent='reddit_media_to_twitter')
+                                  client_secret=reddit_secret,
+                                  user_agent='reddit_media_to_twitter')
         self.reddit_link = url  # TODO validate link here
+        self.submission_id = str()
 
-    def check_host(self):
+    def download_img(self, host):
+        pass
+
+    def download_video(self, host):
+        # TODO check available res
+        # TODO check size of possible downloads
+        # TODO crop functions (?) maybe another class
+        # TODO download file
+        pass
+
+    def grab_id(self):
+        submission_regex = re.search(r"comments/(.*?)/", self.reddit_link)
+        if submission_regex:
+            self.submission_id = submission_regex.group(1)
+        else:
+            self.submission_id = ''
+
+    @staticmethod
+    def check_host(submission_object):
         # TODO check if submission has media
         # TODO check if media is not 404
         # TODO check server
         pass
-
-    def download_img(self):
-        pass
-
-    def download_video(self):
-        pass
-
-    def grab_id(self):
-
-        submission_regex = re.search(r"comments/(.*?)/", self.reddit_link)
-        if submission_regex:
-            submission_id = submission_regex.group(1)
-        else:
-            submission_id = ''
-
-        return submission_id
-
